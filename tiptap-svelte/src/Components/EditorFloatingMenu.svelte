6a8ff7cb388dd4c56605a9be073d30c86260d052
Andrew K <andrewjk@gmail.com>
2019-10-22 14:40:09 +1100

    Add/emit show/hide events in EditorFloatingMenu component (a9db2b2)

--- tiptap-svelte/src/Components/EditorFloatingMenu.svelte
+++ tiptap-svelte/src/Components/EditorFloatingMenu.svelte
@@ -1,5 +1,5 @@
 <script>
-  import { onMount, onDestroy, tick } from "svelte";
+  import { onMount, onDestroy, createEventDispatcher, tick } from "svelte";
   import FloatingMenu from "../Plugins/FloatingMenu";
 
   export let editor;
@@ -11,6 +11,8 @@
     bottom: 0
   };
 
+  const dispatch = createEventDispatcher();
+
   onMount(async () => {
     if (editor) {
       await tick();
@@ -19,6 +21,12 @@
           editor,
           element: el,
           onUpdate: newMenu => {
+            // the second check ensures event is fired only once
+            if (newMenu.isActive && menu.isActive === false) {
+              dispatch("show", newMenu);
+            } else if (!newMenu.isActive && menu.isActive === true) {
+              dispatch("hide", newMenu);
+            }
             menu = newMenu;
           }
         })
