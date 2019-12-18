c5b90c020bed432fda67ac5a885ab19579ff2509
Andrew K <andrewjk@gmail.com>
2019-12-18 12:22:54 +1100

    Fix the link to example code

--- tiptap-svelte-examples/src/routes/_layout.svelte
+++ tiptap-svelte-examples/src/routes/_layout.svelte
@@ -4,6 +4,9 @@
   import Subnavigation from "../components/Subnavigation";
   import Icon from "../components/Icon";
   //import Ad from 'Components/Ad'
+
+  export let segment;
+  $: codeUrl = `https://github.com/andrewjk/tiptap-svelte/tree/master/tiptap-svelte-examples/src/routes/${segment}`;
 </script>
 
 <style lang="scss" src="_layout.scss">
@@ -23,7 +26,7 @@
   </div>
 
   <div class="page__footer">
-    <a class="page__source-link" href="$route.meta.githubUrl" target="_blank">
+    <a class="page__source-link" href={codeUrl} target="_blank">
       <Icon name="code" />
       <span>Show Code</span>
     </a>
